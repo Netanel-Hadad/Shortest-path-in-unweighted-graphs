6862d82a5fecb6238ba32eebc0113263995249a0
Netanel Hadad <netanel.hadad3@gmail.com>
2025-07-05 22:27:25 +0300

    better comments

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+"""
+This file contains different functions for different tasks
+"""
+
+# find a vertex in a graph by its key value
 def findVertexByKey(G, k):
     for v in G.V:
         if v.key == k:
